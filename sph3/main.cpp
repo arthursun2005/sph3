0a13d43146f1b257b23b2939fe69ec2b82357a5f
Arthur Sun <arthursun2005@gmail.com>
2019-03-17 17:20:09 +1100

    Initial Commit

---
+++ sph3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sph3
+//
+//  Created by Arthur Sun on 3/17/19.
+//  Copyright © 2019 Arthur Sun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
